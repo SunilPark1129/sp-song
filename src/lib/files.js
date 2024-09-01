998a84eb5b15bc08e60f2bb67068a6d66ba2adb2
Sunil Park <sunilpark1129@gmail.com>
2024-09-01 16:30:20 -0400

    added - new song

--- src/lib/files.js
+++ src/lib/files.js
@@ -20,6 +20,7 @@ import tree from "../asset/그래도 오길 잘 했어요.wav";
 import tree2 from "../asset/나이든 나무.wav";
 import seens_when from "../asset/언제 부터 였을까.wav";
 import touch_wind from "../asset/다른 이에 닿은 바람.wav";
+import breath from "../asset/숨쉬는 나.wav";
 
 import same_heart_thumb from "../asset/같은 마음이었으면-Thumbnail.jpg";
 import paper_thumb from "../asset/도화지-Thumbnail.jpg";
@@ -41,8 +42,14 @@ import rain_sound_v2_thumb from "../asset/들리는 빗소리-v2-Thumbnail.jpg";
 import boobubble_thumb from "../asset/부우 버블-Thumbnail.jpg";
 import tree_thumb from "../asset/그래도 오길 잘 했어요-Thumbnail.jpg";
 import touch_wind_thumb from "../asset/다른 이에 닿은 바람-Thumbnail.jpg";
+import breath_thumb from "../asset/숨쉬는 나-Thumbnail.jpg";
 
 export const aduioFiles = [
+  {
+    title: "숨쉬는 나",
+    url: breath,
+    thumb: breath_thumb,
+  },
   {
     title: "다른 이에 닿은 바람",
     url: touch_wind,
